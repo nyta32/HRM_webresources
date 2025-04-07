fa2915521a691f455d031c8bbe47fa2fe02b4748
nyta32 <ny.t@freemail.hu>
2025-04-08 00:15:38 +0200

    Add files via upload

--- functions.js
+++ functions.js
@@ -378,11 +378,11 @@ function socketonmessage(event){
 			document.getElementById('websocketRefreshTimemSec').innerText = (data["refr"]/1000)+"s";
 			document.getElementById('refresh_time').innerText = getCurrentTimestamp();
 				 
-			if(0<data["300"]){errors+="reg.: 300="+data["300"]+"<br/>\n";}
-			if(0<data["301"]){errors+="reg.: 301="+data["301"]+"<br/>\n";}
-			if(0<data["302"]){errors+="reg.: 302="+data["302"]+"<br/>\n";}
-			if(0<data["303"]){errors+="reg.: 303="+data["303"]+"<br/>\n";}
-			if(0<data["304"]){errors+="reg.: 304="+data["304"]+"<br/>\n";} 
+			if(0<data["300"]){errors+="reg.300="+data["300"]+"<br/>\n";}
+			if(0<data["301"]){errors+="reg.301="+data["301"]+"<br/>\n";}
+			if(0<data["302"]){errors+="reg.302="+data["302"]+"<br/>\n";}
+			if(0<data["303"]){errors+="reg.303="+data["303"]+"<br/>\n";}
+			if(0<data["304"]){errors+="reg.304="+data["304"]+"<br/>\n";} 
 			
 			if(errors.length && document.getElementById("errors").className == "hide"){
 				document.getElementById("errors").className = "";
